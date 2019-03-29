1b5b5ce88876851cc0eb7eaf29a4923851ed1031
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-03-29 08:47:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
-server_domain='localtest.me'
-server_port='443'
-target_domain="$server_domain"
-target_port='80'
+SERVER_DOMAIN=${SERVER_DOMAIN:-'localtest.me'}
+SERVER_PORT=${SERVER_PORT:-'443'}
+TARGET_DOMAIN=${TARGET_DOMAIN:-"$SERVER_DOMAIN"}
+TARGET_PORT=${TARGET_PORT:-'80'}
 
-target_address="$target_domain"
+target_address="$TARGET_DOMAIN"
 # resolve localhost to dockerhost
 echo "target_address $target_address"
-if [ "$(dig "$target_domain" +short)" == "127.0.0.1" ]; then
+if [ "$(dig "$TARGET_DOMAIN" +short)" == "127.0.0.1" ]; then
   target_address="$(getent hosts host.docker.internal | cut -d' ' -f1)"
   echo "target_address $target_address"
   if [ ! "$target_address" ]; then
@@ -16,7 +16,7 @@ if [ "$(dig "$target_domain" +short)" == "127.0.0.1" ]; then
   fi
 fi
 
-if [ ! -e "/etc/nginx/certificates/${server_domain}.key" ]; then
+if [ ! -e "/etc/nginx/certificates/${SERVER_DOMAIN}.key" ]; then
   # generate self signed certificate
   mkdir -p '/etc/nginx/certificates/'
   temp_certificate_conf="$(mktemp)"
@@ -36,23 +36,24 @@ DNS.1 = ${server_domain}
 DNS.2 = *.${server_domain}
 EOL
 
-openssl genrsa -out "/etc/nginx/certificates/${server_domain}.key" 2048
-openssl req -x509 -new -key "/etc/nginx/certificates/${server_domain}.key" \
+openssl genrsa -out "/etc/nginx/certificates/${SERVER_DOMAIN}.key" 2048
+openssl req -x509 -new -key "/etc/nginx/certificates/${SERVER_DOMAIN}.key" \
   -sha256 -nodes -config "$temp_certificate_conf" \
-  -out "/etc/nginx/certificates/${server_domain}.crt"
+  -out "/etc/nginx/certificates/${SERVER_DOMAIN}.crt"
 
 else
-  echo "Reusing certificate '${server_domain}.crt' and key '${server_domain}.key'"
+  echo "Reusing certificate '${SERVER_DOMAIN}.crt' and key '${SERVER_DOMAIN}.key'"
 fi
 
 # configure nginx
-sed -i "s/%server_domain%/${server_domain}/g" /etc/nginx/conf.d/default.conf
-sed -i "s/%server_port%/${server_port}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/%server_domain%/${SERVER_DOMAIN}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/%server_port%/${SERVER_PORT}/g" /etc/nginx/conf.d/default.conf
 sed -i "s/%target_address%/${target_address}/g" /etc/nginx/conf.d/default.conf
-sed -i "s/%target_port%/${target_port}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/%target_port%/${TARGET_PORT}/g" /etc/nginx/conf.d/default.conf
 
 # run nginx
 echo
-echo "https://$server_domain:$server_port -> http://$target_domain:$target_port"
 nginx -v
+echo
+echo "https://$SERVER_DOMAIN:$SERVER_PORT -> http://$TARGET_DOMAIN:$TARGET_PORT"
 exec nginx -g 'daemon off;'
